98490a4c0c14667b5167d572edc16150bfa62e74
TimeMachine00 <ai.py.syed@gmail.com>
2021-07-19 23:05:34 +0530

    pro-1

---
+++ Project-1.py
@@ -0,0 +1,17 @@
+# created by hussainatphysics@gmail.com(hussainsha syed)
+
+""" In This Project-1 we design full name display
+it will take first name and last name as inputs
+
+it will generate full name
+
+
+"""
+
+firstName= input("Hello dear tell me your first name\n")
+but1=input("press enter")
+lastName= input("hello dear, now tell me your last name\n")
+but2=input("press enter")
+print("Waw your full name very beautifl", firstName + " " + lastName)
+print()
+but3=input("press enter for bye!!!!!!!!!!!!!!!")
